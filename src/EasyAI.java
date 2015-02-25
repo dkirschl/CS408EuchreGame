7f33f8bf4c425d223c8ea73916e7e55afddc19c4
Michael Howard <howard77@purdue.edu>
2015-02-24 19:09:47 -0500

    Started structure of Easy and Medium AI

--- src/EasyAI.java
+++ src/EasyAI.java
@@ -1,24 +1,63 @@
+import java.util.ArrayList;
+
 public class EasyAI extends AI{
+	
+	ArrayList<Card> elCards = new ArrayList<Card>();
+	
 	public EasyAI(){
 		;
 	}
 
+	/*
+	 * 	Easy AI will always pass
+	 */
 	@Override
 	public boolean passOrPickUp() {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
+	/*
+	 * 	Easy AI will always pass. If AI is dealer they pick a random suit
+	 */
 	@Override
 	public char chooseSuit() {
-		// TODO Auto-generated method stub
+		/*
+		if(I am not the dealer){
+			return pass value;
+		}
+		*/
+		
+		/*
+		 * 	Determine which suit cannot be chosen 
+		 */
+		
+		//int suit;
+		/*
+		 * 	Randomly pick one of the remaining suits
+		 */
 		return 0;
 	}
-
+	
+	/*
+	 * 	Easy AI will determine eligible cards and play a random one
+	 */
 	@Override
 	public Card playCard() {
-		// TODO Auto-generated method stub
-		System.out.println("Playing card from easy");
+		//Determine the leading suit
+		
+		/*
+		 for each card in hand:
+		 	if next card in hand's suit == leading suit {
+		 		add card to eligible cards
+		 	}
+		 
+		 if elCards.size() == 0 {
+		 	play random card from hand since they are all eligible
+		  } else {
+		  	play random card from eligible cards since they should all follow suit
+		  }
+		 */
 		return new Card(0, 0, null);
 	}
 }
--- src/MediumAI.java
+++ src/MediumAI.java
@@ -1,24 +1,87 @@
-public class MediumAI extends AI {
+import java.util.ArrayList;
+
+public class MediumAI extends AI{
+	
+	ArrayList<Card> elCards = new ArrayList<Card>();
+	
 	public MediumAI(){
 		;
 	}
 
+	/*
+	 * 	Medium AI will pick up if it has 2 or more of trump in their hand
+	 * 
+	 * 	Possibly make this 2+ if your team deal, 3+ if other team deal 
+	 */
 	@Override
 	public boolean passOrPickUp() {
-		// TODO Auto-generated method stub
+		//int count;
+		
+		//Determine leading suit
+		
+		/*
+		 	for each card in your hand:
+		 		if next card's suit == middle card suit
+		 			count++
+		 			
+		 	if count >= 2 {
+		 		return true;
+		 	} else {
+		 		return false;
+		 	}
+		 */
 		return false;
 	}
 
+	/*
+	 * 	Medium AI will always pass unless they have 3 of the same suit. 
+	 * 	If AI is dealer, they pick the suit they have the most of.
+	 */
 	@Override
 	public char chooseSuit() {
-		// TODO Auto-generated method stub
+		//int hearts, spades, clubs, diamonds;
+		/*
+		 * 	Determine which suit cannot be chosen 
+		 */
+		
+		//int suit;
+		
+		/*
+		 	for each suit that can be picked:
+		 		if next card's suit == hearts(etc.)	(If card is a jack you have to add to both suits
+		 			hearts(etc)++;
+		 		if any suit == 3
+		 			pick the current suit
+		 	
+		 	if I am not the dealer
+		 		pass
+		 	else
+		 		pick the suit with the highest number
+		 */
+		
 		return 0;
 	}
-
+	
+	/*
+	 * 	Medium AI will determine eligible cards and play the highest
+	 * 	Possibly change this to lowest if teammate is winning or highest if opposition is winning
+	 */
 	@Override
 	public Card playCard() {
-		// TODO Auto-generated method stub
-		System.out.println("Playing card from medium");
-		return new Card(0,0,null);
+		//Determine the leading suit
+		
+		/*
+		 for each card in hand:
+		 	if next card in hand's suit == leading suit {
+		 		add card to eligible cards
+		 	}
+		 
+		 if elCards.size() == 0 {
+		 	play lowest card in hand or trump depending on how difficult we want hard to be
+		  } else {
+		  	play highest card that is eligible
+		  }
+		 */
+		return new Card(0, 0, null);
 	}
 }
